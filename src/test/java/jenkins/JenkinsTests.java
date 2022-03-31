7c31c40ea162290826687da6a4a98671045efa3f
Recep Aykurt <recep.aykurt7@gmail.com>
2022-04-01 00:44:07 +0300

    jenkinsTestss

--- src/test/java/jenkins/JenkinsTests.java
+++ src/test/java/jenkins/JenkinsTests.java
@@ -52,4 +52,35 @@ public class JenkinsTests {
     public void apiTest3(){
         System.out.println("*** Jenkins Well ***");
     }
+
+//    static void printArray(int arr[], int n)
+//    {
+//        for (int i = 0; i < n; i++)
+//            System.out.print(arr[i] + " ");
+//        System.out.println();
+//    }
+//    static void RearrangePosNeg(int arr[], int n)
+//    {
+//
+//        int key =0;
+//
+//        for(int i =0; i <n ; i++){
+//            for (int j=i+1; j<n; j++){
+//                if(arr[i]>0){
+//                    key=arr[i];
+//                    arr[i]=arr[j];
+//                    arr[j]=key;
+//                }
+//            }
+//        }
+//
+//    }
+//    public static void main(String[] args)
+//    {
+//        int arr[] = {-12, 11, -13, -5, 6, -7, 5, -3, -6 };
+//        int n = arr.length;
+//        RearrangePosNeg(arr, n);
+//        printArray(arr, n);
+//    }
 }
+
