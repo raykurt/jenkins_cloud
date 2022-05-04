df7e948565da0b3f7eae424280565f50a61d4d0c
Recep Aykurt <recep.aykurt7@gmail.com>
2022-05-04 22:01:45 +0300

    jenkinsTests

---
+++ src/test/java/jenkins/Kelime_Harf_Sayilari.java
@@ -0,0 +1,25 @@
+package jenkins;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Kelime_Harf_Sayilari {
+
+    public static void main(String[] args) {
+        String str = "Yesterday I drove to San Diego and went surfing";
+
+        System.out.println(count(str));
+    }
+
+    static String count(String s){
+        String[] words = s.replaceAll("\\p{Punct}", "").split("\\s+");
+        int wordsCount = words.length;
+        List<Integer> charsInWord = new ArrayList<>();
+        for (String word : words) {
+            charsInWord.add(word.length());
+        }
+        Collections.sort(charsInWord);
+        return "wordcount: "+wordsCount+", wordsizes: "+charsInWord;
+    }
+}
