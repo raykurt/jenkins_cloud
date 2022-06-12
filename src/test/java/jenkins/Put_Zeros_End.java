9318b97c971cc457be07354e684709b67524b2f6
Recep Aykurt <recep.aykurt7@gmail.com>
2022-06-12 22:19:25 +0300

    jenkinsTests

---
+++ src/test/java/jenkins/Put_Zeros_End.java
@@ -0,0 +1,36 @@
+package jenkins;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Put_Zeros_End {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the length of the array...");
+        int length = scan.nextInt();
+        System.out.println("Enter the array elements...");
+
+        int[] arr = new int[length];
+
+        for (int i = 0; i < length; i++) {
+            arr[i] = scan.nextInt();
+        }
+        System.out.println(Arrays.toString(arr));
+
+        int[] brr = new int[length];
+        int idx = 0;
+
+        for (int i = 0; i < length; i++) {
+            if (arr[i] != 0) {
+                brr[idx] = arr[i];
+                idx++;
+            }
+        }
+
+        System.out.println(Arrays.toString(brr));
+
+        scan.close();
+    }
+}
