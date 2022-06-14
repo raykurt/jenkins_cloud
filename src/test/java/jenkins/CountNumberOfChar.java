c58a282cb315dc3ebeabe68d16752d18fb73039e
Recep Aykurt <recep.aykurt7@gmail.com>
2022-06-14 23:18:43 +0300

    jenkinsTests

---
+++ src/test/java/jenkins/CountNumberOfChar.java
@@ -0,0 +1,52 @@
+package jenkins;
+
+public class CountNumberOfChar {
+
+
+    /*
+	 	Get a String and a character from user
+	 	Count the number of characters between the first occurence and the last occurence of the given character in the String
+	 	Do not count the space characters
+	 	If the character which user selected is displayed just once in the String return -1
+	 	If the character which user selected does not exist in the String return -1
+	 	For example; "Java is easy" - 'a' ==> 5
+	 	             "Java is easy" - 'w' ==> -1
+	 	             "Java is easy" - 'e' ==> -1
+	 */
+
+    public static void main(String[] args) {
+
+        System.out.println(getNumOfChars());
+    }
+
+    public static int getNumOfChars() {
+
+        String str = "Java is easy";
+
+        char ch = 'a';
+
+        int firstOccIdx = str.indexOf(ch);
+        int lastOccIdx = str.lastIndexOf(ch);
+        int counter = 0;
+
+        System.out.println(firstOccIdx);
+        System.out.println(lastOccIdx);
+
+        if (firstOccIdx == lastOccIdx) {
+            return -1;
+        } else {
+
+            for (int i = firstOccIdx + 1; i < lastOccIdx; i++) {
+
+                if (str.charAt(i) != ' ') {
+                    counter++;
+                }
+
+            }
+
+            return counter;
+        }
+
+    }
+
+}
