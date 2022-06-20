a25db49acf3720763cbb9f7e2c8c70a68082fae5
Recep Aykurt <recep.aykurt7@gmail.com>
2022-06-20 21:56:41 +0300

    jenkinsTests

--- src/test/java/jenkins/Reverse_Integer.java
+++ src/test/java/jenkins/Reverse_Integer.java
@@ -4,7 +4,7 @@ public class Reverse_Integer {
 
     public static void main(String[] args) {
 
-            int num = 12834, reversed = 0;
+            int num = 128534, reversed = 0;
 
             while(num != 0) {
             int digit = num % 10;
