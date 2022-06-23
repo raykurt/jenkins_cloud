6db4fcfebce229f381068d2627a91f71624a880f
Recep Aykurt <recep.aykurt7@gmail.com>
2022-06-23 23:04:29 +0300

    jenkinsTests

--- src/test/java/jenkins/CountNumberOfChar.java
+++ src/test/java/jenkins/CountNumberOfChar.java
@@ -20,7 +20,7 @@ public class CountNumberOfChar {
 
     public static int getNumOfChars() {
 
-        String str = "Java is easy";
+        String str = "Java is very easy";
 
         char ch = 'a';
 
