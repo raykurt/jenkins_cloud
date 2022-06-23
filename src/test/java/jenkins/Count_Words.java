2d65f362f5f79918c5d9807f436e10be9b0d290b
Recep Aykurt <recep.aykurt7@gmail.com>
2022-06-23 23:01:58 +0300

    jenkinsTests

---
+++ src/test/java/jenkins/Count_Words.java
@@ -0,0 +1,25 @@
+package jenkins;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Count_Words {
+
+    public static void main(String[] args) {
+
+        String str = "Hi Hello How are you welcome to USA Thanks Welcome to USA USA";
+        Map<String, Integer> hashMap = new HashMap<>();
+
+        String[] words = str.split(" ");
+
+        for (String word : words) {
+            Integer integer = hashMap.get(word);
+            if (integer == null)
+                hashMap.put(word, 1);
+            else {
+                hashMap.put(word, integer + 1);
+            }
+        }
+        System.out.println(hashMap);
+    }
+}
