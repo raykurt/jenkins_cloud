5bddab5e6c68036cb66fe01057e9651c256a84f2
Recep Aykurt <recep.aykurt7@gmail.com>
2022-06-26 22:21:12 +0300

    jenkinsTests

--- src/test/java/jenkins/Frequency_Tekrarlayan.java
+++ src/test/java/jenkins/Frequency_Tekrarlayan.java
@@ -6,7 +6,7 @@ public class Frequency_Tekrarlayan {
 
     public static void main(String[] args) {
 
-        int [] arr = new int [] {1, 2, 8, 9, 2, 2, 7, 5, 1};
+        int [] arr = new int [] {1, 2, 8, 9, 2, 3, 7, 5, 1};
         int [] fr = new int [arr.length];
 
         int visited = -1;
