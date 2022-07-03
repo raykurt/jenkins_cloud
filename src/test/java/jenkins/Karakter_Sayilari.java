c1aeeddf004bbc3026e137002cedc5de92618a0d
Recep Aykurt <recep.aykurt7@gmail.com>
2022-07-03 23:27:08 +0300

    jenkinsTests

--- src/test/java/jenkins/Karakter_Sayilari.java
+++ src/test/java/jenkins/Karakter_Sayilari.java
@@ -9,7 +9,7 @@ public class Karakter_Sayilari {
 
         Map<Character, Integer> map = new HashMap<>();
 
-        String str = "aayhnbcbzzy";
+        String str = "aayh24bcbzzy";
 
         for (int i = 0; i < str.length(); i++) {
             char c = str.charAt(i);
