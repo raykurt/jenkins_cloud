90db9eaf35ac8b796ca75d24386d863ea4d3ea0e
Recep Aykurt <recep.aykurt7@gmail.com>
2022-07-29 23:43:16 +0300

    jenkinsTests

---
+++ src/test/java/jenkins/Valid_Parantezis.java
@@ -0,0 +1,38 @@
+package jenkins;
+
+import java.util.Stack;
+
+public class Valid_Parantezis {
+
+    public static void main(String[] args) {
+        System.out.println("Answer : " + CheckParentesis("([]){}()"));
+    }
+
+    public static boolean CheckParentesis(String str)
+    {
+        if (str.isEmpty())
+            return true;
+
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            char current = str.charAt(i);
+            if (current == '{' || current == '(' || current == '[') {
+                stack.push(current);
+            }
+
+
+            if (current == '}' || current == ')' || current == ']') {
+                if (stack.isEmpty())
+                    return false;
+
+                char last = stack.peek();
+                if (current == '}' && last == '{' || current == ')' && last == '(' || current == ']' && last == '[')
+                    stack.pop();
+                else
+                    return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
