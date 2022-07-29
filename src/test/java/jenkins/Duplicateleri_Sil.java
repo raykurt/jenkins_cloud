c30b392680d44d11bc726a64c3714fd081c120ed
Recep Aykurt <recep.aykurt7@gmail.com>
2022-07-29 23:45:06 +0300

    jenkinsTests

--- src/test/java/jenkins/Duplicateleri_Sil.java
+++ src/test/java/jenkins/Duplicateleri_Sil.java
@@ -4,7 +4,7 @@ public class Duplicateleri_Sil {
 
     public static void main(String[] args) {
 
-        String str = "abG2137cW24c21";
+        String str = "abG2137sscW24c21";
         String result = "";
         for (int i = 0; i < str.length(); i++) {
             if(!result.contains(String.valueOf(str.charAt(i)))) {
