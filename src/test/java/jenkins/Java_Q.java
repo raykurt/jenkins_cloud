cfea808208fbac99b92d5ecc072227f29d91db6c
Recep Aykurt <recep.aykurt7@gmail.com>
2022-07-31 23:14:55 +0300

    jenkinsTests

---
+++ src/test/java/jenkins/Java_Q.java
@@ -0,0 +1,38 @@
+package jenkins;
+
+public class Java_Q {
+
+    public static void main(String[] args) {
+
+        String num = "032149808";
+
+        int sum = 10;
+        int total = 0;
+        String lastDigit= "0";
+
+        if (num.length() !=9 ){
+            System.out.println("Number is not 9 digit");
+        } else {
+            for (int i=0; i<9; i++){
+                if (num.charAt(i)=='0'){
+                    sum--;
+                }else {
+                    total += sum*Integer.parseInt(num.substring(i,i+1));
+                    sum--;
+                }
+            }
+
+            if (total%11==0){
+                System.out.println(num+"0");
+            }else {
+                for (int i=1; i<11; i++){
+                    if ((total+i)%11==0){
+                        lastDigit= String.valueOf(i);
+                        break;
+                    }
+                }
+                System.out.println(num+lastDigit);
+            }
+        }
+    }
+}
---
+++ src/test/java/jenkins/Java_Q2.java
@@ -0,0 +1,9 @@
+package jenkins;
+
+public class Java_Q2 {
+
+    public static void main(String[] args) {
+
+
+    }
+}
