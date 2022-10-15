ead4fdfb526c65251a6613ee77e2f498886d0db4
Recep <recep.aykurt7@gmail.com>
2022-10-15 20:25:43 +0300

    "Med_DB_Api"

--- src/test/java/jenkins/EmptyFile.java
+++ src/test/java/jenkins/EmptyFile.java
@@ -7,10 +7,10 @@ public class EmptyFile {
 
         for (int i = 0; i < 10; i++) {
             for (int j = i; j < 5; j++) {
-                System.out.print("#_");
+                System.out.print("& ");
             }
             for (int k=4-i; k<0; k++) {
-                System.out.print("#_");
+                System.out.print("& ");
             }
             System.out.println();
         }
