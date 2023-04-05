c7809aec1c41cde0d7d3d939c81556bb400c4c7c
Recep <raykurt@hotmail.com>
2023-04-05 14:51:13 +0300

    Commit 1

--- src/test/java/jenkins/Sum_Of_Digits.java
+++ src/test/java/jenkins/Sum_Of_Digits.java
@@ -4,7 +4,7 @@ public class Sum_Of_Digits {
 
     public static void main(String[] args) {
 
-        	int number = 19894;
+        	int number = 195424;
             int sum = 0;
             while(number > 0) {
                 sum += number%10;
